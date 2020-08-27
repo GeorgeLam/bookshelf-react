57825ade1e815223887daf72190bcf7c61fb2dc1
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-08-27 21:35:13 +0100

    Cleaned code

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,15 +10,6 @@ let booksShown = 0;
 const Search = () => {
     const { accStatus, setAccStatus } = useContext(AccContext);
 
-    // this.handleSearchInput = this.handleSearchInput.bind(this);
-    // this.searchBookFunc = this.searchBookFunc.bind(this);
-    // this.searchAuthorFunc = this.searchAuthorFunc.bind(this);
-    // this.navPrev = this.navPrev.bind(this);
-    // this.navForward = this.navForward.bind(this);
-    // // this.saveMeth = this.saveMeth.bind(this);
-    // this.saveMethod = this.saveMethod.bind(this);
-
-
     let [type, setType] = useState("");
     let [index, setIndex] = useState(0);
     let [data, setData] = useState("");
@@ -34,23 +25,6 @@ const Search = () => {
     let [tidied, setTidied] = useState("");
     let [newSave, setNewSave] = useState("");
     let [savedBooks, setSavedBooks] = useState(JSON.parse(localStorage.getItem('books')) || []);
-   
-
-  //   this.state = {
-  //     type: "",
-  //     index: 0,
-  //     data: "",
-  //     loaded: false,
-  //     searchQuery: "",
-  //     searchStatus: "",
-  //     url: "",
-  //     startIndex: 0,
-  //     foundItems: 0,
-  //     tidied: "",
-  //     newSave: "",
-  //     savedBooks: JSON.parse(localStorage.getItem('books')) || [],
-  //   };
-  // }
 
   let searchBookFunc = () => {
     console.log(searchInput);
@@ -59,17 +33,6 @@ const Search = () => {
     setIndex(0)
     setStartIndex(0);
 
-    //searcher();
-
-    // this.setState(
-    //   (prevState) => ({
-    //     searchQuery: prevState.searchInput,
-    //     type: "book",
-    //     index: 0,
-    //   }),
-    //   () => this.searcher()
-    // );
-    // this.searcher();
   }
 
     useEffect(() => {
@@ -87,8 +50,7 @@ const Search = () => {
 
     useEffect(() => {
       console.log(url);
-      // setUrl(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&startIndex=${startIndex}`);
-      // searcher();
+
       if(url) searcher();
 
     }, [url]);
@@ -102,26 +64,12 @@ const Search = () => {
     setIndex(0);
     setStartIndex(0);
 
-    //searcher();
-
-
-    // this.setState(
-    //   (prevState) => ({
-    //     searchQuery: prevState.searchInput,
-    //     type: "author",
-    //     index: 0,
-    //   }),
-    //   () => this.searcher()
-    // );
   }
 
   let handleSearchInput = (e) => {
     e.preventDefault();
     setSearchInput(e.target.value);
 
-    // this.setState({
-    //   searchInput: e.target.value,
-    // });
   }
 
   let searcher = async () => {
@@ -129,82 +77,32 @@ const Search = () => {
     setSearchStatus("Fetching data...");
     setLoaded(false)
     console.log(searchInput);
-
-    // this.setState({
-    //   searchStatus: "Fetching data...",
-    //   loaded: false,
-    // }
   
 
     if (searchInput) {
       if (type == "book") {
         console.log("type is book", searchQuery, startIndex)
-        // setUrl(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&startIndex=${startIndex}`);
       } else if (type == "author") {
         console.log("author")
-        // setUrl(`https://www.googleapis.com/books/v1/volumes?q=+inauthor:${searchQuery}&startIndex=${startIndex}`);
+
       }
 
       console.log(url)
 
       let response = await fetch(url);
-      //let data = await response.json();
       console.log(data);
       setData(await response.json());
       setLoaded(true);
 
-      // this.setState({
-      //   data: data,
-      //   loaded: true,
-      //   totalItems: data.totalItems,
-      // });
+
       console.log(data.items);
       console.log(loaded);
       setReadyToTidy(true);
-      // let catsup = data.items.map((book) => {
-      //   console.log(book);
-      //   if (book?.volumeInfo?.authors?.length > 1) {
-      //     console.log(book.volumeInfo.authors);
-      //     book.volumeInfo.authors = book.volumeInfo.authors.join(" and ");
-      //   }
-      //   if (book?.volumeInfo?.publishedDate?.length > 4) {
-      //     book.volumeInfo.publishedDate = book.volumeInfo.publishedDate
-      //       .toString()
-      //       .slice(0, 4);
-      //   }
-      //   if (book?.volumeInfo?.description?.length > 300) {
-      //     book.volumeInfo.description =
-      //       book.volumeInfo.description.slice(0, 300) + "...";
-      //   }
-      // })
-      
-      // console.log(catsup);
+    
       }
     }
 
-    // useEffect(() => {
-    //   data && setReadyToTidy(true)
-    // }, [data])
-
-  // let tidySearchResults = () => {
-  //   setTidied(data.items.map((book) => {
-  //       console.log(book);
-  //       if (book?.volumeInfo?.authors?.length > 1) {
-  //         console.log(book.volumeInfo.authors);
-  //         book.volumeInfo.authors = book.volumeInfo.authors.join(" and ");
-  //       }
-  //       if (book?.volumeInfo?.publishedDate?.length > 4) {
-  //         book.volumeInfo.publishedDate = book.volumeInfo.publishedDate
-  //           .toString()
-  //           .slice(0, 4);
-  //       }
-  //       if (book?.volumeInfo?.description?.length > 300) {
-  //         book.volumeInfo.description =
-  //           book.volumeInfo.description.slice(0, 300) + "...";
-  //       }
-  //     })
-  //   )
-  // }
+    
 
   useEffect(() => {
     if (data) {
@@ -230,38 +128,11 @@ const Search = () => {
   }, [data])
 
 
-  //   this.setState({
-  //     tidied: this.state.data.items.map((book) => {
-  //       //console.log(book);
-  //       if (book?.volumeInfo?.authors?.length > 1) {
-  //         console.log(book.volumeInfo.authors);
-  //         book.volumeInfo.authors = book.volumeInfo.authors.join(" and ");
-  //       }
-  //       if (book?.volumeInfo?.publishedDate?.length > 4) {
-  //         book.volumeInfo.publishedDate = book.volumeInfo.publishedDate
-  //           .toString()
-  //           .slice(0, 4);
-  //       }
-  //       if (book?.volumeInfo?.description?.length > 300) {
-  //         book.volumeInfo.description =
-  //           book.volumeInfo.description.slice(0, 300) + "...";
-  //       }
-  //     }),
-  //   });
-  // }
-
   let navPrev = () => {
     if (startIndex != 0) {
       setStartIndex(startIndex - 10);
       setLoaded(false)
       searcher();
-      // this.setState(
-      //   (prevState) => ({
-      //     startIndex: prevState.startIndex - 10,
-      //     loaded: false,
-      //   }),
-      //   () => this.searcher()
-      // );
     }
   }
 
@@ -271,13 +142,6 @@ const Search = () => {
     setLoaded(false);
     searcher();
 
-    // this.setState(
-    //   (prevState) => ({
-    //     startIndex: prevState.startIndex + 10,
-    //     loaded: false,
-    //   }),
-    //   () => this.searcher()
-    // );
   }
 
   let saveMethod = (pageBookNum, savedReview, savedRating) => {
@@ -299,26 +163,7 @@ const Search = () => {
     )
 
     localStorage.setItem("books", JSON.stringify(savedBooks));
-    // setIsOpen(false);
-
-    // this.setState(
-    //   (prevState) => {
-    //     savedBooks: prevState.savedBooks.push({
-    //       title: this.state.data.items[pageBookNum].volumeInfo.title,
-    //       authors: this.state.data.items[pageBookNum].volumeInfo.authors,
-    //       date: this.state.data.items[pageBookNum].volumeInfo.publishedDate,
-    //       image: this.state.data.items[pageBookNum].volumeInfo.imageLinks
-    //         .smallThumbnail,
-    //       id: this.state.data.items[pageBookNum].id,
-    //       learnLink: `https://books.google.com/books?id=${this.state.data.items[pageBookNum].id}`,
-    //       rating: savedRating,
-    //       review: savedReview,
-    //     });
-    //   }, () => {
-    //     localStorage.setItem("books", JSON.stringify(this.state.savedBooks));
-    //   }
-    // );
-    // this.isOpen = false;
+    
   }
 
     return (
